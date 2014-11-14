1b949aa81b197a36a049b4196222049277ff694e
xhwang <xhwang@chromium.org>
2014-11-14 13:01:20 -0800

    Move Encrypted Media classes from /content to /media.
    File move and format cleanup only. No functionality change.
    
    List of classes moved:
    - CdmSessionAdapter
    - WebContentDecryptionModuleImpl
    - WebContentDecryptionModuleSessionImpl
    
    BUG=422730
    
    Review URL: https://codereview.chromium.org/726803002
    
    Cr-Original-Commit-Position: refs/heads/master@{#304264}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 58bc11c76ea016ff24bb9e50c1662c5d628b5521

---
+++ blink/webcontentdecryptionmodule_impl.h
@@ -0,0 +1,80 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BLINK_WEBCONTENTDECRYPTIONMODULE_IMPL_H_
+#define MEDIA_BLINK_WEBCONTENTDECRYPTIONMODULE_IMPL_H_
+
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/strings/string16.h"
+#include "media/base/media_export.h"
+#include "third_party/WebKit/public/platform/WebContentDecryptionModule.h"
+#include "third_party/WebKit/public/platform/WebContentDecryptionModuleResult.h"
+
+namespace blink {
+#if defined(ENABLE_PEPPER_CDMS)
+class WebLocalFrame;
+#endif
+class WebSecurityOrigin;
+}
+
+namespace media {
+
+class CdmFactory;
+class CdmSessionAdapter;
+class Decryptor;
+class MediaKeys;
+class WebContentDecryptionModuleSessionImpl;
+
+class MEDIA_EXPORT WebContentDecryptionModuleImpl
+    : public blink::WebContentDecryptionModule {
+ public:
+  static WebContentDecryptionModuleImpl* Create(
+      CdmFactory* cdm_factory,
+      const blink::WebSecurityOrigin& security_origin,
+      const base::string16& key_system);
+
+  virtual ~WebContentDecryptionModuleImpl();
+
+  // Returns the Decryptor associated with this CDM. May be NULL if no
+  // Decryptor associated with the MediaKeys object.
+  // TODO(jrummell): Figure out lifetimes, as WMPI may still use the decryptor
+  // after WebContentDecryptionModule is freed. http://crbug.com/330324
+  Decryptor* GetDecryptor();
+
+#if defined(ENABLE_BROWSER_CDMS)
+  // Returns the CDM ID associated with this object. May be kInvalidCdmId if no
+  // CDM ID is associated, such as when Clear Key is used.
+  int GetCdmId() const;
+#endif  // defined(ENABLE_BROWSER_CDMS)
+
+  // blink::WebContentDecryptionModule implementation.
+  virtual blink::WebContentDecryptionModuleSession* createSession();
+  // TODO(jrummell): Remove this method once blink updated.
+  virtual blink::WebContentDecryptionModuleSession* createSession(
+      blink::WebContentDecryptionModuleSession::Client* client);
+
+  virtual void setServerCertificate(
+      const uint8* server_certificate,
+      size_t server_certificate_length,
+      blink::WebContentDecryptionModuleResult result);
+
+ private:
+  // Takes reference to |adapter|.
+  WebContentDecryptionModuleImpl(scoped_refptr<CdmSessionAdapter> adapter);
+
+  scoped_refptr<CdmSessionAdapter> adapter_;
+
+  DISALLOW_COPY_AND_ASSIGN(WebContentDecryptionModuleImpl);
+};
+
+// Allow typecasting from blink type as this is the only implementation.
+inline WebContentDecryptionModuleImpl* ToWebContentDecryptionModuleImpl(
+    blink::WebContentDecryptionModule* cdm) {
+  return static_cast<WebContentDecryptionModuleImpl*>(cdm);
+}
+
+}  // namespace media
+
+#endif  // MEDIA_BLINK_WEBCONTENTDECRYPTIONMODULE_IMPL_H_
